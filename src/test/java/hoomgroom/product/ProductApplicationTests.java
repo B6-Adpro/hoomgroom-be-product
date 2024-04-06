a58f98badb652f90c45724ec6cc4c1deb0f2a502
Matthew <matthewhjt72@gmail.com>
2024-04-06 20:19:49 +0700

    init

---
+++ src/test/java/hoomgroom/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package hoomgroom.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
