193dba51e60565e0c0906560d0d59998b7f5661d
Matthew <matthewhjt72@gmail.com>
2024-04-24 20:25:33 +0700

    [GREEN] Implement FixedAmountPromo.java

---
+++ src/main/java/hoomgroom/product/Promo/model/FixedAmountPromo.java
@@ -0,0 +1,10 @@
+package hoomgroom.product.Promo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FixedAmountPromo extends Promo {
+    Long discountAmount;
+}
