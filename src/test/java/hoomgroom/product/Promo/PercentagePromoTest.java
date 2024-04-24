bade5f8aa3801c958ba7c88d52b3c99226e32e80
Matthew <matthewhjt72@gmail.com>
2024-04-24 20:32:47 +0700

    [REFACTOR] Implement PercentagePromoFactory in PercentagePromoTest.java

--- src/test/java/hoomgroom/product/Promo/PercentagePromoTest.java
+++ src/test/java/hoomgroom/product/Promo/PercentagePromoTest.java
@@ -1,5 +1,6 @@
 package hoomgroom.product.Promo;
 
+import hoomgroom.product.Promo.model.Factory.PercentagePromoFactory;
 import hoomgroom.product.Promo.model.PercentagePromo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,9 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PercentagePromoTest {
     PercentagePromo promo;
 
+    PercentagePromoFactory factory = new PercentagePromoFactory();
+
     @BeforeEach
     void setUp(){
-        this.promo = new PercentagePromo();
+        this.promo = factory.createPromo();
         this.promo.setUuid(UUID.fromString("898e0bf5-9fa3-4815-92a5-0e9c422f9c3a"));
         this.promo.setName("BELANJAHEMAT20");
         this.promo.setDescription("Promo ini mengurangi harga sebesar 20 persen");
