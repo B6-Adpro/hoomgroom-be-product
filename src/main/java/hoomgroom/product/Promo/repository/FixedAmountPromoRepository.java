edbf26b75fac745f14502982cc8ea783d0b11d5b
Matthew <matthewhjt72@gmail.com>
2024-05-06 00:47:28 +0700

    refactor: add @NonNull annotation for repository.findById Id field

--- src/main/java/hoomgroom/product/Promo/repository/FixedAmountPromoRepository.java
+++ src/main/java/hoomgroom/product/Promo/repository/FixedAmountPromoRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FixedAmountPromoRepository extends JpaRepository<FixedAmountPromo, UUID> {
+    Optional<FixedAmountPromo> findById(@NonNull UUID id);
     List<FixedAmountPromo> findByName(@NonNull String name);
 
     @Query("SELECT p FROM FixedAmountPromo p WHERE p.expirationDate <= :dateTime")
--- src/main/java/hoomgroom/product/Promo/repository/PercentagePromoRepository.java
+++ src/main/java/hoomgroom/product/Promo/repository/PercentagePromoRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+
 public interface PercentagePromoRepository extends JpaRepository<PercentagePromo, UUID> {
+    Optional<PercentagePromo> findById(@NonNull UUID id);
     List<PercentagePromo> findByName(@NonNull String name);
+
 
     @Query("SELECT p FROM PercentagePromo p WHERE p.expirationDate <= :dateTime")
     List<PercentagePromo> findByExpirationDateBefore(@NonNull LocalDateTime dateTime);
