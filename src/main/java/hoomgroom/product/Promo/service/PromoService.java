3565b88510501b61c277273e05ce91a6312aea9c
Matthew <matthewhjt72@gmail.com>
2024-05-26 21:30:41 +0700

    refactor: promo service

--- src/main/java/hoomgroom/product/Promo/service/PromoService.java
+++ src/main/java/hoomgroom/product/Promo/service/PromoService.java
@@ -1,14 +1,23 @@
-package hoomgroom.product.Promo.service;
+package hoomgroom.product.promo.service;
 
-import hoomgroom.product.Promo.model.Promo;
+import hoomgroom.product.promo.dto.PromoResponse;
+import hoomgroom.product.promo.dto.RedeemResponse;
+import hoomgroom.product.promo.dto.Response;
+import hoomgroom.product.promo.model.Promo;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public interface PromoService {
-    List<Promo> findAll();
-    Promo findById(UUID uuid);
-    void delete(UUID uuid);
+    CompletableFuture<List<Promo>> findAll();
+    CompletableFuture<ResponseEntity<PromoResponse>> findById(UUID uuid);
+    CompletableFuture<ResponseEntity<Response>> delete(UUID uuid) throws ExecutionException, InterruptedException;
+    CompletableFuture<ResponseEntity<RedeemResponse>> redeem(UUID transactionId, UUID promoId, Long totalPrice);
+
+    boolean isValid (Promo promo);
 }
