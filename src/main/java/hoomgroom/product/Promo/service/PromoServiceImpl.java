4ec596a240e9c088170ddb90889c08e78dfa03cb
Matthew <matthewhjt72@gmail.com>
2024-05-26 22:15:07 +0700

    refactor: refactor promo service

--- src/main/java/hoomgroom/product/Promo/service/PromoServiceImpl.java
+++ src/main/java/hoomgroom/product/Promo/service/PromoServiceImpl.java
@@ -1,43 +1,102 @@
-package hoomgroom.product.Promo.service;
+package hoomgroom.product.promo.service;
 
-import hoomgroom.product.Promo.model.Promo;
-import hoomgroom.product.Promo.repository.PromoRepository;
+import hoomgroom.product.promo.dto.PromoResponse;
+import hoomgroom.product.promo.dto.RedeemResponse;
+import hoomgroom.product.promo.dto.Response;
+import hoomgroom.product.promo.model.Promo;
+import hoomgroom.product.promo.repository.PromoRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class PromoServiceImpl implements PromoService {
+    private static final String PROMO_NOT_FOUND_MESSAGE = "Promo id %s not found!";
     private final PromoRepository promoRepository;
 
-    @Override
     public List<Promo> findAll() {
         return promoRepository.findAll();
     }
 
-    @Override
-    public Promo findById(@NonNull UUID id) throws NoSuchElementException {
+    public ResponseEntity<PromoResponse> findById(@NonNull UUID id){
         Optional<Promo> promo = promoRepository.findById(id);
-        if (promo.isEmpty()){
-            throw new NoSuchElementException();
+        if (promo.isEmpty()) {
+            PromoResponse response = PromoResponse.builder()
+                    .message(String.format(PROMO_NOT_FOUND_MESSAGE, id))
+                    .build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
-        return promo.get();
+
+        PromoResponse response = PromoResponse
+                .builder()
+                .message("Success")
+                .promo(promo.get())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
-    @Override
-    public void delete(UUID id) {
+    public ResponseEntity<Response> delete(UUID id){
         Optional<Promo> promo = promoRepository.findById(id);
 
         if (promo.isEmpty()) {
-            throw new NoSuchElementException();
+            Response response = Response.builder()
+                    .message(String.format(PROMO_NOT_FOUND_MESSAGE, id))
+                    .build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
 
         promoRepository.delete(promo.get());
+        Response response = Response.builder()
+                .message(String.format("Promo id %s deleted successfully", promo.get().getId().toString()))
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    public boolean isValid (Promo promo) {
+        return promo.getExpirationDate().isBefore(LocalDateTime.now());
+    }
+
+    public ResponseEntity<RedeemResponse> redeem(UUID transactionId, UUID promoId, Long totalPrice) {
+        Optional<Promo> promo = promoRepository.findById(promoId);
+        if (promo.isEmpty()) {
+            RedeemResponse response = RedeemResponse.builder()
+                    .message(String.format(PROMO_NOT_FOUND_MESSAGE, promoId))
+                    .transactionId(transactionId)
+                    .promoId(promoId)
+                    .normalPrice(totalPrice)
+                    .discountPrice(totalPrice)
+                    .build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        } else if (!isValid(promo.get())) {
+            RedeemResponse response = RedeemResponse.builder()
+                    .message(String.format("Promo id %s has expired!", promoId))
+                    .transactionId(transactionId)
+                    .promoId(promoId)
+                    .normalPrice(totalPrice)
+                    .discountPrice(totalPrice)
+                    .build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        Long discountPrice =  promo.get().applyPromo(totalPrice);
+
+        RedeemResponse response = RedeemResponse.builder()
+                .message(String.format("Success redeem promo %s", promoId))
+                .transactionId(transactionId)
+                .promoId(promoId)
+                .normalPrice(totalPrice)
+                .discountPrice(discountPrice)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 }
