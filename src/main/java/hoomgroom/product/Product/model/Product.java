ddc82fda7cd02df5163975422dbabb469db2c5f0
ferupk <feru.pratama@ui.ac.id>
2024-05-26 22:16:07 +0700

    [REFACTOR] Set ORM definition for Product model

--- src/main/java/hoomgroom/product/Product/model/Product.java
+++ src/main/java/hoomgroom/product/Product/model/Product.java
@@ -1,23 +1,43 @@
 package hoomgroom.product.Product.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
     private UUID id;
+
+    @Column(name = "name")
     private String name;
+
+    @ElementCollection
+    @Column(name = "tags")
     private List<String> tags;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "image_link")
     private String imageLink;
+
+    @Column(name = "original_price")
     private Long originalPrice;
+
+    @Column(name = "discount_percentage")
     private Integer discountPercentage;
+
+    @Column(name = "total_sales")
     private Integer totalSales;
 
     public Long getDiscountedPrice() {
