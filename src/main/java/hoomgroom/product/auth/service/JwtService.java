d33cefb6811906cb94f53904437e6d59c6904f75
Matthew <matthewhjt72@gmail.com>
2024-05-31 01:25:09 +0700

    refactor: remove isTokenExpired

--- src/main/java/hoomgroom/product/auth/service/JwtService.java
+++ src/main/java/hoomgroom/product/auth/service/JwtService.java
@@ -5,15 +5,12 @@ import io.jsonwebtoken.Claims;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
-import java.util.Date;
 import java.util.function.Function;
 
 public interface JwtService {
     String extractUsername(String token);
     User extractUser(String token);
     boolean isTokenValid(String token, UserDetails userDetails);
-    boolean isTokenExpired(String token);
-    Date extractExpiration(String token);
     <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
     Claims extractAllClaims(String token);
     Key getSignInKey();
